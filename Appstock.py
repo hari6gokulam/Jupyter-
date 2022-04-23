20f80761871b69586589f1c8f4b73ea494fe444d
hari6gokulam <65290238+hari6gokulam@users.noreply.github.com>
2022-04-23 14:05:12 +0530

    Add files via upload

---
+++ Appstock.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+***Nthu Myraano aavo***
+
+""")
+tickerSymbol = 'AAPL'
+#https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2019-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
